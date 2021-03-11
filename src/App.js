7e08077f223c6a199d5f2b03e1a5b5e7de27d583
saleh <sjaffal3@gmail.com>
2021-03-12 00:19:46 +0400

    installed axios and imported into app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    
+    axios({
+      url: 'https://api.unsplash.com/search/photos',
+      params: {
+        client_id: 'DVkpqhjTJAEw1t_glS_nIrrxXdxoC32jbO6F3FoEtjA',
+        query: 'puppies',
+        per_page: 30
+      }
+    }).then( (res) => {
+      
+      
+    })
+
+  })
+
   return (
     <h1>nextBinge</h1>
   );
