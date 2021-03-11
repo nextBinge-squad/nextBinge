fc17305a542e08bef694a0ada89877664bb30074
LeonBaram <leonbaram@protonmail.com>
2021-03-11 14:37:57 -0500

    set up firebase config, created firebase project bucket

---
+++ src/firebase-config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-3OAbUS60zS9ZUCJOUCudx4LM8l-wsz8",
+  authDomain: "nextbinge-1bb04.firebaseapp.com",
+  projectId: "nextbinge-1bb04",
+  storageBucket: "nextbinge-1bb04.appspot.com",
+  messagingSenderId: "838443696855",
+  appId: "1:838443696855:web:e66773730bd329c7006692"
+};
+firebase.initalizeApp(firebaseConfig);
+
+const pathref = path => firebase.database().ref(path);
+const dbref = firebase.database().ref();
+
+export default firebase;
+export { dbref, pathref };
