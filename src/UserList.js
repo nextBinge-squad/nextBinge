bf9d61a351a12cad794327154fe7bb9b2db9f5eb
LeonBaram <leonbaram@protonmail.com>
2021-03-13 16:32:11 -0500

    added tvShows prop to UserList

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,13 @@
-function UserList() {
-  return(<>
-  </>);
+// tvShows should be an array of tvShow objects
+function UserList({ tvShows }) {
+  return(
+    <ul className="UserList">
+      {tvShows.map(tvShow => 
+        <li className="tvShow" key={tvShow.id.firebase}>
+
+        </li> )}
+    </ul>
+  );
 }
 
 export default UserList;
