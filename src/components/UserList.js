9a9aef1b5eccbac44835dcac5237267ab272bec1
LeonBaram <leonbaram@protonmail.com>
2021-03-13 17:29:38 -0500

    added some comments

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -3,13 +3,16 @@ import RenderTvShow from "./RenderTvShow";
 // tvShows should be an array of tvShow objects
 function UserList({ tvShows }) {
 
+  // defines a sorting criterion: sort based on tv shows' "priority" property
   const compareShows = (tvShow1, tvShow2) =>
     tvShow2.priority - tvShow1.priority;
 
   return (
     <ul className="UserList">
       {tvShows
+        // sort tvShows based on compareShows criterion
         .sort(compareShows)
+        // render each tv show using RenderTvShow component
         .map(tvShow =>
           <li className="tvShow" key={tvShow.id.firebase}>
             <RenderTvShow tvShow={tvShow} />
