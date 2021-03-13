743324b6284e0623aab9fed503f130c6f34a9cfc
LeonBaram <leonbaram@protonmail.com>
2021-03-13 17:24:56 -0500

    added priority display to RenderTvShow for demo purposes

--- src/components/RenderTvShow.js
+++ src/components/RenderTvShow.js
@@ -1,8 +1,15 @@
+import { Fragment } from "react";
+
 function RenderTvShow({ tvShow }) {
   return (
-    <div className="tvShow" dangerouslySetInnerHTML={{ __html: tvShow.summary }}>
-
-    </div>
+    <>
+      <div
+        className="tvShow"
+        dangerouslySetInnerHTML={{ __html: tvShow.summary }}
+      >
+      </div>
+      <p>priority: {tvShow.priority}</p>
+    </>
   )
 }
 
