919a57d5e17830b18698c4447cb8c6269a436c1d
saleh <sjaffal3@gmail.com>
2021-03-19 05:24:58 +0400

    removed routing functionality

--- src/App.js
+++ src/App.js
@@ -7,19 +7,12 @@ import firebase, { dbref, pathref } from './firebase-config';
 // components
 import UserInput from './components/UserInput';
 import TVCardSmall from './components/TVCardSmall';
-import TVCardBig from './components/TVCardBig';
 import BingeList from './components/BingeList';
 // 3rd party
 // axios
 import axios from 'axios';
 // sweet alert
 import Swal from 'sweetalert2';
-// react router dom
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
 
 function App() {
 
@@ -30,22 +23,17 @@ function App() {
   }, []);
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
       <div className="nextBinge wrapper">
         <header>
-          <Link to={'/'}><h1 className="title">nextBinge</h1></Link>
+         <h1 className="title">nextBinge</h1>
         </header>
 
         <UserInput />
-
-        <Route path="/TVCardSmall" exact component={TVCardSmall} />
-        <Route path="/TVCardSmall/TVCardBig" exact component={TVCardBig} />
 
         <footer>
           <p>Created at <a href="https://www.junocollege.com">Juno College</a> 2021 by Leon Baram, Sal Jaffal & Lawrence Lee</p>
         </footer>
       </div>
-    </Router>
   );
 }
 
